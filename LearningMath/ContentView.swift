45341f33539bdaea68edf53ba496ed965e976dbd
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2022-02-03 19:38:28 -0500

    Initial Commit

---
+++ LearningMath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningMath
+//
+//  Created by Gerard Gomez on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningMath/LearningMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningMathApp.swift
+//  LearningMath
+//
+//  Created by Gerard Gomez on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
