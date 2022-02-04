2adbd8d1a374c716003ac7d1a0f7d8d36ff48a9f
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2022-02-03 20:29:52 -0500

    -Add core data model -Add core data class -Created a homeView and ContentView

---
+++ LearningMath/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  LearningMath
+//
+//  Created by Gerard Gomez on 2/3/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationBarTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .environmentObject(DataController())
+    }
+}
